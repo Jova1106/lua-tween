fde769c8813e12fbfc9268f6023cd8420330af19
Jova1106 <jova1106@gmail.com>
2025-01-29 07:21:59 -0500

    prevent conflicts between multiple tween addon instances

--- lua/autorun/tween.lua
+++ lua/autorun/tween.lua
@@ -22,6 +22,8 @@
 
 --------------------------------------------------------------------------------
 
+if tween then return end
+
 tween = {}
 
 local isfunction = isfunction
@@ -65,10 +67,10 @@ TWEEN_EASE_BOUNCE_IN_OUT = math.ease.InOutBounce
 local function table_Inherit(target, base)
 	for k, v in next, base do
 		if target[k] then continue end
-		
+
 		target[k] = v
 	end
-	
+
 	return target
 end
 
@@ -76,38 +78,38 @@ local metaTable_Vector2 = {
 	__add = function(self, other)
 		return Vector2(self.x + other.x, self.y + other.y)
 	end,
-	
+
 	__sub = function(self, other)
 		return Vector2(self.x - other.x, self.y - other.y)
 	end,
-	
+
 	__mul = function(self, other)
 		if isnumber(other) then
 			return Vector2(self.x * other, self.y * other)
 		elseif isnumber(self) then
 			return Vector2(self * other.x, self * other.y)
 		end
-		
+
 		return Vector2(self.x * other.x, self.y * other.y)
 	end,
-	
+
 	__div = function(self, other)
 		if isnumber(other) then
 			return Vector2(self.x / other, self.y / other)
 		end
-		
+
 		return Vector2(self.x / other.x, self.y / other.y)
 	end,
-	
+
 	__tostring = function(self)
 		return string.format("%.6f %.6f", self.x, self.y)
 	end,
-	
+
 	SetUnpacked = function(self, x, y)
 		self.x = x
 		self.y = y
 	end,
-	
+
 	GetPos = function(self)
 		return Vector2(self.x, self.y)
 	end
@@ -124,7 +126,7 @@ function Vector2(x, y)
 		x = x,
 		y = y
 	}
-	
+
 	return setmetatable(Vector2, metaTable_Vector2)
 end
 
@@ -136,7 +138,7 @@ local Lerp = tween.Lerp
 
 local function CalcAng(from, to)
 	local diff = math_fmod(to - from, 360)
-	
+
 	return math_fmod(2 * diff, 360) - diff
 end
 
@@ -156,13 +158,13 @@ local LerpAngle = tween.LerpAngle
 
 local function LerpVector2Unpacked(vector2, from, to, t)
 	local lerped_vector2 = Lerp(from, to, t)
-	
+
 	vector2:SetUnpacked(lerped_vector2.x, lerped_vector2.y)
 end
 
 local function LerpVectorUnpacked(vector, from, to, t)
 	local lerped_vector = Lerp(from, to, t)
-	
+
 	vector:SetUnpacked(lerped_vector.x, lerped_vector.y, lerped_vector.z)
 end
 
@@ -194,24 +196,24 @@ end
 
 local function BinomialCoefficient(n, k)
 	local result = 1
-	
+
 	for i = 1, k do
 		result = result * (n - i + 1) / i
 	end
-	
+
 	return result
 end
 
 function tween.BSpline(points, t)
 	local n = #points
 	local result = Vector_0
-	
+
 	for i = 1, n do
 		local weight = BinomialCoefficient(n - 1, i - 1) * math_pow(1 - t, n - i) * math_pow(t, i - 1)
-		
+
 		result = result + weight * points[i]
 	end
-	
+
 	return result
 end
 
@@ -248,7 +250,7 @@ local metaTable_Tween = {
 	__newindex = function(self, key, value)
 		rawset(self, key, value)
 	end,
-	
+
 	Start = function(self)
 		self.running = true
 		self.start_time = SysTime()
@@ -256,143 +258,143 @@ local metaTable_Tween = {
 		self.time_left = self.duration
 		self.tween_type = tween_type(self.from)
 		self.lerp_type = type_to_function[self.tween_type]
-		
+
 		all_tweens[self] = true
 		running_tweens[self] = true
 	end,
-	
+
 	SetPermanent = function(self, bool)
 		self.permanent = bool
 	end,
-	
+
 	SetFrom = function(self, from)
 		self.from = from
 	end,
-	
+
 	SetTo = function(self, to)
 		self.to = to
 	end,
-	
+
 	SetWaypoints = function(self, from, to)
 		self.from = from
 		self.to = to
 	end,
-	
+
 	SetDuration = function(self, duration)
 		self.duration = duration
 	end,
-	
+
 	SetEaseType = function(self, ease_type)
 		self.ease_type = ease_type
 	end,
-	
+
 	Restart = function(self)
 		if !all_tweens[self] then
 			self:Start()
-			
+
 			return
 		end
-		
+
 		self.running = true
 		self.start_time = SysTime()
 		self.end_time = self.start_time + self.duration
 		self.time_left = self.duration
-		
+
 		if !running_tweens[self] then
 			running_tweens[self] = true
 		end
-		
+
 		if paused_tweens[self] then
 			paused_tweens[self] = nil
 		elseif stopped_tweens[self] then
 			stopped_tweens[self] = nil
 		end
 	end,
-	
+
 	Pause = function(self)
 		if stopped_tweens[self] then return end
-		
+
 		self.running = false
-		
+
 		if running_tweens[self] then
 			running_tweens[self] = nil
 			paused_tweens[self] = true
 		end
 	end,
-	
+
 	Resume = function(self)
 		if stopped_tweens[self] then
 			self:Restart()
-			
+
 			return
 		end
-		
+
 		self.start_time = SysTime() - (self.duration - self.time_left)
 		self.end_time = self.start_time + self.duration
 		self.running = true
-		
+
 		if paused_tweens[self] then
 			paused_tweens[self] = nil
 			running_tweens[self] = true
 		end
 	end,
-	
+
 	Stop = function(self)
 		self.running = false
-		
+
 		if running_tweens[self] then
 			running_tweens[self] = nil
 		elseif paused_tweens[self] then
 			paused_tweens[self] = nil
 		end
-		
+
 		stopped_tweens[self] = true
 	end,
-	
+
 	Update = function(self)
 		if self.running then
 			local time = SysTime()
 			self.time_left = self.end_time - time
-			
+
 			if time >= self.end_time then
 				self.running = false
 				self.value = self.to
-				
+
 				if !self.permanent then
 					all_tweens[self] = nil
 					running_tweens[self] = nil
 				end
-				
+
 				if self.callback != nil then
 					self.callback(self)
 				end
-				
+
 				return
 			end
-			
+
 			local alpha = (time - self.start_time) / self.duration
-			
+
 			self.value = self.lerp_type(self.from, self.to, self.ease_type(alpha))
-			
+
 			local OnUpdate = self.OnUpdate
-			
+
 			if OnUpdate and isfunction(OnUpdate) then
 				self:OnUpdate()
 			end
 		end
 	end,
-	
+
 	TimeLeft = function(self)
 		return self.time_left
 	end,
-	
+
 	GetValue = function(self)
 		return self.value
 	end,
-	
+
 	Destroy = function(self)
 		all_tweens[self] = nil
-		
+
 		if running_tweens[self] then
 			running_tweens[self] = nil
 		elseif paused_tweens[self] then
@@ -401,7 +403,7 @@ local metaTable_Tween = {
 			stopped_tweens[self] = nil
 		end
 	end,
-	
+
 	SetCallback = function(self, callback)
 		self.callback = callback
 	end
@@ -421,7 +423,7 @@ function Tween(from, to, duration, ease_type, callback)
 		permanent = false,
 		running = false
 	}
-	
+
 	return setmetatable(Tween, metaTable_Tween)
 end
 
@@ -433,36 +435,36 @@ local metaTable_TweenUnpacked = {
 		self.time_left = self.duration
 		self.tween_type = tween_type(self.base_object)
 		self.lerp_type_unpacked = type_to_function_unpacked[self.tween_type]
-		
+
 		all_tweens[self] = true
 		running_tweens[self] = true
 	end,
-	
+
 	Update = function(self)
 		if self.running then
 			local to = self.to
 			local time = SysTime()
 			self.time_left = self.end_time - time
-			
+
 			if time >= self.end_time then
 				self.running = false
 				self.value = to
 				self.base_object = to
-				
+
 				if !self.permanent then
 					all_tweens[self] = nil
 					running_tweens[self] = nil
 				end
-				
+
 				if self.callback != nil then
 					self.callback()
 				end
-				
+
 				return
 			end
-			
+
 			local alpha = (time - self.start_time) / self.duration
-			
+
 			if self.tween_type == "number" then
 				self.value = Lerp(self.from, to, self.ease_type(alpha))
 			else
@@ -470,9 +472,9 @@ local metaTable_TweenUnpacked = {
 				self.lerp_type_unpacked(base_object, self.from, to, self.ease_type(alpha))
 				self.value = base_object
 			end
-			
+
 			local OnUpdate = self.OnUpdate
-			
+
 			if OnUpdate and isfunction(OnUpdate) then
 				self:OnUpdate()
 			end
@@ -497,7 +499,7 @@ function TweenUnpacked(base_object, from, to, duration, ease_type, callback)
 		permanent = false,
 		running = false
 	}
-	
+
 	return setmetatable(Tween, metaTable_TweenUnpacked)
 end
 
@@ -508,39 +510,39 @@ local metaTable_BezierTween = {
 		self.end_time = self.start_time + self.duration
 		self.time_left = self.duration
 		self.value = self.points[1]
-		
+
 		all_tweens[self] = true
 		running_tweens[self] = true
 	end,
-	
+
 	Update = function(self)
 		if self.running then
 			local points = self.points
 			local time = SysTime()
 			self.time_left = self.end_time - time
-			
+
 			if time >= self.end_time then
 				self.running = false
 				self.value = points[#points]
-				
+
 				if !self.permanent then
 					all_tweens[self] = nil
 					running_tweens[self] = nil
 				end
-				
+
 				if self.callback != nil then
 					self.callback(self)
 				end
-				
+
 				return
 			end
-			
+
 			local alpha = (time - self.start_time) / self.duration
-			
+
 			self.value = BSpline(points, self.ease_type(alpha))
-			
+
 			local OnUpdate = self.OnUpdate
-			
+
 			if OnUpdate and isfunction(OnUpdate) then
 				self:OnUpdate()
 			end
@@ -563,16 +565,16 @@ function BezierTween(points, duration, ease_type, callback)
 		permanent = false,
 		running = false,
 	}
-	
+
 	return setmetatable(Tween, metaTable_BezierTween)
 end
 
 hook.Add("Think", "process_tweens", function()
 	if table.IsEmpty(running_tweens) then return end
-	
+
 	for tween in next, running_tweens do
 		if tween == nil then continue end
-		
+
 		tween:Update()
 	end
 end)
